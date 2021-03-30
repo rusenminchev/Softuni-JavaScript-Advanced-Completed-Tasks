7843fba7de642cab97b95313331fb7ad34b011e4
Rusen Minchev <rusenminchev@gmail.com>
2021-03-31 00:20:41 +0300

    add

---
+++ 01. Syntax, Functions and Statements - Lab/07. Day of Week.js
@@ -0,0 +1,20 @@
+function getDayOfWeekAsANumber(input){
+
+let result = 0;
+
+switch(input){
+    case "Monday": result = 1; break;
+    case "Tuesday": result = 2; break;
+    case "Wednesday": result = 3; break;
+    case "Thursday": result = 4; break;
+    case "Friday": result = 5; break;
+    case "Sathurday": result = 6; break;
+    case "Sunday": result = 7; break;
+    default: result = 'error'; break;
+}
+
+console.log(result);
+
+}
+
+getDayOfWeekAsANumber('Ivan');
