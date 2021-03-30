aea74706cdd54575148a0e2796a0b7093569f9e1
Rusen Minchev <rusenminchev@gmail.com>
2021-03-31 00:20:21 +0300

    add

---
+++ 01. Syntax, Functions and Statements - Lab/03. Largest Number.js
@@ -0,0 +1,7 @@
+function getLargestNumber(firstNum, secondNum, thirdNum){
+    let largestNumber = Math.max(firstNum, secondNum, thirdNum);
+    
+    console.log('The largest number is ' + largestNumber + '.');
+    }
+    
+    getLargestNumber(5, -3, 16);
