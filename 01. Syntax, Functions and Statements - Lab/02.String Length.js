dd7e1c50e8f842e9d108e93ff91db429a4876c47
Rusen Minchev <rusenminchev@gmail.com>
2021-03-31 00:20:15 +0300

    add

---
+++ 01. Syntax, Functions and Statements - Lab/02.String Length.js
@@ -0,0 +1,10 @@
+function solve(firstString, secondString, thirdString){
+
+let sumOfTheLenght = firstString.length + secondString.length + thirdString.length;
+let averageLenght = Math.floor(sumOfTheLenght / 3);
+
+console.log(sumOfTheLenght);
+console.log(averageLenght);
+}
+
+stringLengthSum('chocolate', 'ice cream', 'cake');
