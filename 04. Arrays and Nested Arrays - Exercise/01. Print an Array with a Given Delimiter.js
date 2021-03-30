c547e0dc2e3604355007c86bbadfe8d7bacab9b0
Rusen Minchev <rusenminchev@gmail.com>
2021-03-31 00:23:31 +0300

    add

---
+++ 04. Arrays and Nested Arrays - Exercise/01. Print an Array with a Given Delimiter.js
@@ -0,0 +1,13 @@
+function printArrayWithAGivenDelimiter(array, delimiter) {
+
+    console.log(array.join(delimiter));
+
+}
+
+printArrayWithAGivenDelimiter(['How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_');
